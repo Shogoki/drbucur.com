1d539c5ef85960e42f44633e824472e2faa04fde
Sven Kraus <mail@svenkraus.de>
2019-09-10 12:52:59 +0200

    added images to static_paths

--- publishconf.py
+++ publishconf.py
@@ -16,7 +16,7 @@ RELATIVE_URLS = False
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-STATIC_PATHS = ['CNAME']
+STATIC_PATHS = ['CNAME', 'images']
 
 DELETE_OUTPUT_DIRECTORY = True
 
